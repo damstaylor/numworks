b603dd08caf0a509e678fc2138be7bbb8e5bbf84
damstaylor <damiensabo@live.fr>
2023-12-27 16:29:19 +0100

    Create local logic for red-page component

--- src/pages/red-page.vue
+++ src/pages/red-page.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="red-page">
+    <h2>Add days</h2>
+    <div>This is the weekdays list, you can add days to it:</div>
+    <ul>
+      <li v-for="(item, idx) in weekdays" :key="idx">
+        {{ item }}
+      </li>
+    </ul>
+    <input value="fieldValue" @input="onInput" />
+    <button @click="onEnter">Enter</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'red-page',
+  data() {
+    return {
+      fieldValue: '',
+      weekdays: [],
+    };
+  },
   methods: {
+    onInput(event) {
+      this.fieldValue = event.target.value;
+    },
+    onEnter() {
+      this.weekdays.push(this.fieldValue);
+      this.fieldValue = "";
+    },
   },
 };
 </script>
