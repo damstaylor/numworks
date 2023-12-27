b45d95ec481eacb07ae633ea02baf7c5cec4dfa3
damstaylor <damiensabo@live.fr>
2023-12-27 17:18:32 +0100

    Implement game in Blue page

--- src/pages/blue-page.vue
+++ src/pages/blue-page.vue
@@ -1,12 +1,58 @@
 <template>
   <div class="blue-page">
+    <h2>Lets' play a game!</h2>
+    <div>Click on the button in the allotedd time. Each time you click on the button less than 1 second before the alloted time, you get 100 extra points.</div>
+    <div>Score: {{ getScore }}</div>
+    <button v-if="!isStarted" @click="start">Start</button>
+    <button v-else @click="stop">Stop</button>
+    <div v-if="message">{{ message }}</div>
   </div>
 </template>
 
 <script>
+import { useScoreStore } from '../store/score';
+
+const store = useScoreStore();
+
 export default {
   name: 'blue-page',
+  data() {
+    return {
+      isStarted: false,
+      isStopped: false,
+      message: '',
+      timeout: null,
+    };
+  },
+  computed: {
+    getCount() {
+      return store.count;
+    },
+    getScore() {
+      return store.score;
+    },
+  },
   methods: {
+    incrementScore() {
+      store.incrementScore();
+    },
+    start() {
+      this.isStarted = true;
+      this.message = '';
+      this.timeout = setTimeout(() => {
+        if (this.isStopped) {
+          this.message = 'You won!';
+          this.incrementScore();
+        } else {
+          this.message = 'You lost!';
+        }
+        this.isStarted = false;
+        this.isStopped = false;
+      }, 1000);
+    },
+    stop() {
+      this.isStopped = true;
+    },
   },
 };
 </script>
---
+++ src/store/score.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia';
+
+export const useScoreStore = defineStore('score', {
+  state: () => ({
+    score: 0,
+  }),
+  getters: {
+    getScore: (state) => state.score,
+  },
+  actions: {
+    incrementScore() {
+      this.score += 100;
+    },
+  },
+})
